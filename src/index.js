67536aea903d14716561ce5f90895c17e80ef95f
sidiousvic <victor6rene@gmail.com>
2019-05-14 20:07:38 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errMessage: "" };
-    console.log(this.state);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { lat: null, errMessage: "" };
+  //   console.log(this.state);
+  // }
 
   state = { lat: null, errMessage: "" };
 
@@ -25,7 +26,7 @@ class App extends React.Component {
     if (!this.state.errMessage && !this.state.lat) {
       return <div>LOADING...</div>;
     } else if (!this.state.err && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     } else if (this.state.errMessage) {
       return <div>Error: {this.state.errMessage}</div>;
     }
