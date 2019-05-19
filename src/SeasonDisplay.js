22943571650c7092397da640b46e7ac2566fe0e8
sidiousvic <victor6rene@gmail.com>
2019-05-19 22:26:52 +0900

    v1.0

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,16 +1,58 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
-const getSeason = (lat, month) => {
-  if (month > 2 && month < 6) {
-    return lat > 0 ? "Spring" : "Fall";
-  } else if (month > 5 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter";
+const getSeasonData = (lat, month) => {
+  console.log(month);
+  const season = {};
+  if (month === 3 || month === 4 || month === 5) {
+    lat > 0
+      ? (season.text = "Ume are blossoming") &&
+        (season.image = "../seasonImages/spring.gif") &&
+        (season.season = "spring")
+      : (season.text = "Momiji are falling") &&
+        (season.image = "../seasonImages/fall.gif") &&
+        (season.season = "Fall");
+  } else if (month === 6 || month === 7 || month === 8) {
+    lat > 0
+      ? (season.text = "Koi are swimming") &&
+        (season.image = "./seasonImages/summer.gif") &&
+        (season.season = "summer")
+      : (season.text = "Snow is piling") &&
+        (season.image = "./seasonImages/winter.gif") &&
+        (season.season = "winter");
+  } else if (month === 9 || month === 10 || month === 11) {
+    lat < 0
+      ? (season.text = "Ume are blossoming") &&
+        (season.image = "../seasonImages/spring.gif") &&
+        (season.season = "spring")
+      : (season.text = "Momiji are falling") &&
+        (season.image = "../seasonImages/fall.gif") &&
+        (season.season = "fall");
+  } else if (month === 12 || month === 1 || month === 2) {
+    lat < 0
+      ? (season.text = "Koi are swimming") &&
+        (season.image = "./seasonImages/summer.gif") &&
+        (season.season = "summer")
+      : (season.text = "Snow is piling") &&
+        (season.image = "./seasonImages/winter.gif") &&
+        (season.season = "winter");
   }
+  console.log(season);
+  return season;
 };
 
 const SeasonDisplay = props => {
-  const season = getSeason(props.lat, new Date().getMonth());
-  return <div>Season: {season}</div>;
+  const season = getSeasonData(props.lat, 9);
+
+  return (
+    <div className="season-display">
+      <h1>
+        It's {season.season}, {season.text}
+        <span className="blinkellipsis">...</span>
+      </h1>
+      <img alt="season" src={season.image} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
