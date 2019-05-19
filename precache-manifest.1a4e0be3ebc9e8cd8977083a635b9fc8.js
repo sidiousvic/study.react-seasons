7458e915e057928ee470e5f5c8605d08ba1242ff
sidiousvic <victor6rene@gmail.com>
2019-05-19 23:00:18 +0900

    Updates

---
+++ precache-manifest.1a4e0be3ebc9e8cd8977083a635b9fc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf0c516e8eddc8f46264a041176f96e",
+    "url": "/seasons-kisetsu/index.html"
+  },
+  {
+    "revision": "287d5424a0443ef0c6a1",
+    "url": "/seasons-kisetsu/static/css/main.0739f0db.chunk.css"
+  },
+  {
+    "revision": "00326bae65bf152b5d9a",
+    "url": "/seasons-kisetsu/static/js/2.2514f1ab.chunk.js"
+  },
+  {
+    "revision": "287d5424a0443ef0c6a1",
+    "url": "/seasons-kisetsu/static/js/main.0ecfb1fc.chunk.js"
+  },
+  {
+    "revision": "65d8adec05826ffb8852",
+    "url": "/seasons-kisetsu/static/js/runtime~main.471414c2.js"
+  },
+  {
+    "revision": "3549beaae0ba185e62d53e57144caa0d",
+    "url": "/seasons-kisetsu/static/media/fall.3549beaa.gif"
+  },
+  {
+    "revision": "c40db678aa354ba91257f6258a1521e1",
+    "url": "/seasons-kisetsu/static/media/spring.c40db678.gif"
+  },
+  {
+    "revision": "9298b8aa90e9bc71a6162878ee24cbeb",
+    "url": "/seasons-kisetsu/static/media/summer.9298b8aa.gif"
+  },
+  {
+    "revision": "23ab9479b2f555bda9af1f71329ef902",
+    "url": "/seasons-kisetsu/static/media/winter.23ab9479.gif"
+  }
+]);
