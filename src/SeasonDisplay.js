527a3335faa6dc564f91b078495327bd4059cedc
sidiousvic <victor6rene@gmail.com>
2019-05-19 22:49:40 +0900

    v1.2

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./SeasonDisplay.css";
+import springimg from "./seasonImages/spring.gif";
+import summerimg from "./seasonImages/summer.gif";
+import fallimg from "./seasonImages/fall.gif";
+import winterimg from "./seasonImages/winter.gif";
 
 const getSeasonData = (lat, month) => {
   console.log(month);
@@ -7,34 +11,34 @@ const getSeasonData = (lat, month) => {
   if (month === 3 || month === 4 || month === 5) {
     lat > 0
       ? (season.text = "Ume are blossoming") &&
-        (season.image = "../seasonImages/spring.gif") &&
+        (season.image = springimg) &&
         (season.season = "spring")
       : (season.text = "Momiji are falling") &&
-        (season.image = "../seasonImages/fall.gif") &&
+        (season.image = fallimg) &&
         (season.season = "Fall");
   } else if (month === 6 || month === 7 || month === 8) {
     lat > 0
       ? (season.text = "Koi are swimming") &&
-        (season.image = "./seasonImages/summer.gif") &&
+        (season.image = summerimg) &&
         (season.season = "summer")
       : (season.text = "Snow is piling") &&
-        (season.image = "./seasonImages/winter.gif") &&
+        (season.image = winterimg) &&
         (season.season = "winter");
   } else if (month === 9 || month === 10 || month === 11) {
     lat < 0
       ? (season.text = "Ume are blossoming") &&
-        (season.image = "../seasonImages/spring.gif") &&
+        (season.image = springimg) &&
         (season.season = "spring")
       : (season.text = "Momiji are falling") &&
-        (season.image = "../seasonImages/fall.gif") &&
+        (season.image = fallimg) &&
         (season.season = "fall");
   } else if (month === 12 || month === 1 || month === 2) {
     lat < 0
       ? (season.text = "Koi are swimming") &&
-        (season.image = "./seasonImages/summer.gif") &&
+        (season.image = summerimg) &&
         (season.season = "summer")
       : (season.text = "Snow is piling") &&
-        (season.image = "./seasonImages/winter.gif") &&
+        (season.image = winterimg) &&
         (season.season = "winter");
   }
   console.log(season);
