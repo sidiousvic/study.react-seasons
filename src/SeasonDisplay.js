21a39e7b934be13ccedadd7457c889d33c3b4920
sidiousvic <victor6rene@gmail.com>
2019-05-21 22:44:41 +0900

    Added season switch icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -5,56 +5,47 @@ import summerimg from "./seasonImages/summer.gif";
 import fallimg from "./seasonImages/fall.gif";
 import winterimg from "./seasonImages/winter.gif";
 
-const getSeasonData = (lat, month) => {
+const getSeasonData = (lat, month, season) => {
   console.log("Month: ", month);
-  const season = {};
+  const seasons = {};
   if (month === 3 || month === 4 || month === 5) {
-    lat > 0
-      ? (season.text = "Ume are blossoming") &&
-        (season.image = springimg) &&
-        (season.season = "spring")
-      : (season.text = "Momiji are falling") &&
-        (season.image = fallimg) &&
-        (season.season = "Fall");
+    lat > 0 ? (seasons.season = "spring") : (seasons.season = "fall");
   } else if (month === 6 || month === 7 || month === 8) {
-    lat > 0
-      ? (season.text = "Koi are swimming") &&
-        (season.image = summerimg) &&
-        (season.season = "summer")
-      : (season.text = "Snow is piling") &&
-        (season.image = winterimg) &&
-        (season.season = "winter");
+    lat > 0 ? (seasons.season = "summer") : (seasons.season = "winter");
   } else if (month === 9 || month === 10 || month === 11) {
-    lat < 0
-      ? (season.text = "Ume are blossoming") &&
-        (season.image = springimg) &&
-        (season.season = "spring")
-      : (season.text = "Momiji are falling") &&
-        (season.image = fallimg) &&
-        (season.season = "fall");
+    lat < 0 ? (seasons.season = "spring") : (seasons.season = "fall");
   } else if (month === 12 || month === 1 || month === 2) {
-    lat < 0
-      ? (season.text = "Koi are swimming") &&
-        (season.image = summerimg) &&
-        (season.season = "summer")
-      : (season.text = "Snow is piling") &&
-        (season.image = winterimg) &&
-        (season.season = "winter");
+    lat < 0 ? (seasons.season = "summer") : (seasons.season = "winter");
   }
-  console.log(season.season);
-  return season;
+
+  if (season != null) {
+    seasons.season = season;
+  }
+
+  console.log(seasons.season);
+  return seasons;
 };
 
 const SeasonDisplay = props => {
-  const season = getSeasonData(props.lat, new Date().getMonth());
+  const seasons = getSeasonData(props.lat, props.month, props.season);
+
+  if (seasons.season === "spring") {
+    (seasons.text = "Ume are blossoming") && (seasons.image = springimg);
+  } else if (seasons.season === "summer") {
+    (seasons.text = "Koi are swimming") && (seasons.image = summerimg);
+  } else if (seasons.season === "fall") {
+    (seasons.text = "Momiji are falling") && (seasons.image = fallimg);
+  } else if (seasons.season === "winter") {
+    (seasons.text = "Snow is piling") && (seasons.image = winterimg);
+  }
 
   return (
     <div className="season-display">
       <h1>
-        It's {season.season}, {season.text}
+        It's {seasons.season}, {seasons.text}
         <span className="blink-ellipsis">...</span>
       </h1>
-      <img alt="season" src={season.image} />
+      <img alt="season" src={seasons.image} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import "./index.css";
 
 class App extends React.Component {
-  state = { lat: null, errMessage: "" };
+  state = {
+    lat: null,
+    month: new Date().getMonth(),
+    season: null,
+    errMessage: ""
+  };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -26,7 +32,53 @@ class App extends React.Component {
         </div>
       );
     } else if (!this.state.err && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
+      return (
+        <div>
+          <SeasonDisplay
+            lat={this.state.lat}
+            month={this.state.month}
+            season={this.state.season}
+          />
+          <div className="buttons">
+            <div
+              onClick={() => this.setState({ season: "spring" })}
+              className="ui vertical  button"
+              tabIndex="0"
+            >
+              <div className="visible content">
+                <i class="umbrella icon" />
+              </div>
+            </div>
+            <div
+              onClick={() => this.setState({ season: "summer" })}
+              className="ui vertical  button"
+              tabIndex="0"
+            >
+              <div className="visible content">
+                <i class="sun icon" />
+              </div>
+            </div>
+            <div
+              onClick={() => this.setState({ season: "fall" })}
+              className="ui vertical  button"
+              tabIndex="0"
+            >
+              <div className="visible content">
+                <i class="leaf icon" />
+              </div>
+            </div>
+            <div
+              onClick={() => this.setState({ season: "winter" })}
+              className="ui vertical  button"
+              tabIndex="0"
+            >
+              <div className="visible content">
+                <i class="snowflake icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     } else if (this.state.errMessage) {
       return <div>Error: {this.state.errMessage}</div>;
     }
